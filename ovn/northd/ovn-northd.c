cbbbf69928b47c89fb536d9bb3d381616661bbad
Justin Pettit <jpettit@ovn.org>
2016-06-02 21:44:38 -0700

    ovn-util: Add string representations to 'lport_addresses'.
    A future commit will reduce the amount of conversions used by the
    existing users of 'lport_addresses'.  This change will also make it
    possible to use this structure for logical router port networks.

--- ovn/northd/ovn-northd.c
+++ ovn/northd/ovn-northd.c
@@ -1142,8 +1142,7 @@ build_port_security_nd(struct ovn_port *op, struct hmap *lflows)
                           ds_cstr(&match), "next;");
             ds_destroy(&match);
         }
-        free(ps.ipv4_addrs);
-        free(ps.ipv6_addrs);
+        destroy_lport_addresses(&ps);
     }
 
     char *match = xasprintf("inport == %s && (arp || nd)", op->json_key);
@@ -1245,7 +1244,6 @@ build_port_security_ip(enum ovn_pipeline pipeline, struct ovn_port *op,
             ds_put_cstr(&match, "}");
             ovn_lflow_add(lflows, op->od, stage, 90, ds_cstr(&match), "next;");
             ds_destroy(&match);
-            free(ps.ipv4_addrs);
         }
 
         if (ps.n_ipv6_addrs) {
@@ -1273,8 +1271,9 @@ build_port_security_ip(enum ovn_pipeline pipeline, struct ovn_port *op,
             ovn_lflow_add(lflows, op->od, stage, 90,
                           ds_cstr(&match), "next;");
             ds_destroy(&match);
-            free(ps.ipv6_addrs);
         }
+
+        destroy_lport_addresses(&ps);
 
         char *match = xasprintf(
             "%s == %s && %s == "ETH_ADDR_FMT" && ip", port_direction,
@@ -1283,6 +1282,7 @@ build_port_security_ip(enum ovn_pipeline pipeline, struct ovn_port *op,
         ovn_lflow_add(lflows, op->od, stage, 80, match, "drop;");
         free(match);
     }
+
 }
 
 static bool
@@ -1603,7 +1603,7 @@ build_lswitch_flows(struct hmap *datapaths, struct hmap *ports,
                 free(actions);
             }
 
-            free(laddrs.ipv4_addrs);
+            destroy_lport_addresses(&laddrs);
         }
     }
 
@@ -2306,7 +2306,7 @@ build_lrouter_flows(struct hmap *datapaths, struct hmap *ports,
                     }
                 }
 
-                free(laddrs.ipv4_addrs);
+                destroy_lport_addresses(&laddrs);
             }
         } else if (!strcmp(op->nbs->type, "router")) {
             /* This is a logical switch port that connects to a router. */
